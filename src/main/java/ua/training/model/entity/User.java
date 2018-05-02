da36a3a11fe1745acb705188df45335803f9f555
Roydgar <Roydgaryshka@gmail.com>
2018-05-03 00:14:57 +0300

    Create entities.

--- src/main/java/ua/training/model/entity/User.java
+++ src/main/java/ua/training/model/entity/User.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class User {
     private int id;
-    private String username;
+    private String login;
     private String password;
     private Role role;
 
@@ -12,16 +12,14 @@ public class User {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId(int id) { this.id = id; }
+
+    public String getLogin() {
+        return login;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+    public void setLogin(String login) {
+        this.login = login;
     }
 
     public String getPassword() {
@@ -48,21 +46,21 @@ public class User {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return id == user.id &&
-                Objects.equals(username, user.username) &&
+                Objects.equals(login, user.login) &&
                 Objects.equals(password, user.password) &&
                 role == user.role;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, password, role);
+        return Objects.hash(id, login, password, role);
     }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", username='" + username + '\'' +
+                ", username='" + login + '\'' +
                 ", password='" + password + '\'' +
                 ", role=" + role +
                 '}';
@@ -83,8 +81,8 @@ public class User {
             return this;
         }
 
-        public UserBuilder setUsername(String name) {
-            user.setUsername(name);
+        public UserBuilder setLogin(String login) {
+            user.setLogin(login);
             return this;
         }
 
