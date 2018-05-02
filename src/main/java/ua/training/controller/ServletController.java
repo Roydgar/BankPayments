0a9e881d00c929036e195fca19810308e6924fd0
Roydgar <Roydgaryshka@gmail.com>
2018-05-02 22:34:17 +0300

    Create servlet and project structure.

---
+++ src/main/java/ua/training/controller/ServletController.java
@@ -0,0 +1,43 @@
+package ua.training.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class ServletController extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+
+        path = path.replaceAll(".*/" , "");
+
+        String page = "index.jsp";
+        System.out.println(page);
+
+        if(page.contains("redirect")){
+            response.sendRedirect(page.replace("redirect:", ""));
+        }else {
+            request.getRequestDispatcher(page).forward(request, response);
+        }
+    }
+
+    @Override
+    public void destroy(){
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<html>
+<head>
+    <title>HEELLLLOOO</title>
+
+</head>
+<body>
+
+</body>
+</html>
