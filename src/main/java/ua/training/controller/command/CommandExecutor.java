0f94a3ab6ecc48305b6c924baf873f6dea71d0ad
Roydgar <Roydgaryshka@gmail.com>
2018-05-06 21:42:19 +0300

    Create command executor

---
+++ src/main/java/ua/training/controller/command/CommandExecutor.java
@@ -0,0 +1,28 @@
+package ua.training.controller.command;
+
+import ua.training.controller.command.login.Login;
+import ua.training.controller.command.login.Logout;
+import ua.training.controller.command.login.Registration;
+import ua.training.model.service.UserService;
+import ua.training.util.constants.CommandNames;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CommandExecutor {
+    private Map<String, Command> commands = new ConcurrentHashMap<>();
+
+    public CommandExecutor() {
+        UserService userService = new UserService();
+
+        commands.put(CommandNames.LOGIN, new Login(userService));
+        commands.put(CommandNames.REGISTRATION, new Registration(userService));
+        commands.put(CommandNames.LOGOUT, new Logout());
+    }
+
+
+    public String executeCommand(String command, HttpServletRequest request) {
+        return commands.getOrDefault(command, (r)->"index.jsp").execute(request);
+    }
+}
---
+++ src/main/java/ua/training/util/constants/CommandNames.java
@@ -0,0 +1,7 @@
+package ua.training.util.constants;
+
+public interface CommandNames {
+    String LOGIN        = "login";
+    String REGISTRATION = "registration";
+    String LOGOUT       = "logout";
+}
