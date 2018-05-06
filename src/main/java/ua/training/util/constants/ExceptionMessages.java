48c35f53743b967e0d3582b4e90a9147819c65c0
Roydgar <Roydgaryshka@gmail.com>
2018-05-06 20:52:30 +0300

    Add response and exception messages constants

---
+++ src/main/java/ua/training/util/constants/ExceptionMessages.java
@@ -0,0 +1,5 @@
+package ua.training.util.constants;
+
+public interface ExceptionMessages {
+    String LOGIN_FAILED     = "User with given login and password doesn't exist";
+}
---
+++ src/main/java/ua/training/util/constants/ResponseMessages.java
@@ -0,0 +1,12 @@
+package ua.training.util.constants;
+
+import java.util.ResourceBundle;
+
+public interface ResponseMessages {
+
+    String LOGIN_USER_IS_LOGGED         = "login.error.userIsLogged";
+    String LOGIN_ERROR                  = "login.error";
+
+    String REGISTRATION_PASSWORDS_DONT_MATCH    = "registration.error.passwordsDontMatch";
+    String REGISTRATION_USER_EXISTS             = "registration.error.userExists";
+}
