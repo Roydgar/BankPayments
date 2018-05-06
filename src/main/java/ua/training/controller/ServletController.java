63425f5105da81fb48613a3299bb8daa79622a16
Roydgar <Roydgaryshka@gmail.com>
2018-05-06 21:41:59 +0300

    Refactor servlet controller

--- src/main/java/ua/training/controller/ServletController.java
+++ src/main/java/ua/training/controller/ServletController.java
@@ -1,26 +1,16 @@
 package ua.training.controller;
 
-import ua.training.controller.command.Command;
-import ua.training.controller.command.CommandCreator;
-import ua.training.controller.command.login.Login;
-import ua.training.controller.command.login.Logout;
-import ua.training.controller.command.login.Registration;
-import ua.training.model.service.UserService;
-import ua.training.util.constants.AttributeNames;
+import ua.training.controller.command.CommandExecutor;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class ServletController extends HttpServlet {
-    private CommandCreator commandCreator = new CommandCreator();
+    private CommandExecutor commandExecutor = new CommandExecutor();
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
     }
@@ -44,7 +34,7 @@ public class ServletController extends HttpServlet {
         path = path.replaceAll(".*/" , "");
 
         System.out.println(path);
-        String page = commandCreator.executeCommand(path, request);
+        String page = commandExecutor.executeCommand(path, request);
         System.err.println(page);
 
         request.getRequestDispatcher(page).forward(request, response);
