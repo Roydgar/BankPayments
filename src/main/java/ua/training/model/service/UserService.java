07a9d9056ec31cee3d5737ccddcd72774cb3e46a
Roydgar <Roydgaryshka@gmail.com>
2018-05-06 18:40:09 +0300

    Create user service to work with dao

---
+++ src/main/java/ua/training/model/service/UserService.java
@@ -0,0 +1,63 @@
+package ua.training.model.service;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import ua.training.exception.UserDoesntExistException;
+import ua.training.model.dao.DaoFactory;
+import ua.training.model.dao.UserDao;
+import ua.training.model.entity.User;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
+import java.util.List;
+
+public class UserService {
+    private UserDao userDao = DaoFactory.getInstance().createUserDao();
+    public void create(String login, String password, User.Role role, String email) {
+        userDao.create(new User.UserBuilder().setLogin(login).setPassword(encryptPassword(password))
+        .setEmail(email).setRole(role).create());
+    }
+
+    public User findById(int id) {
+        return null;
+    }
+
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    public void update(User entity) {
+        userDao.update(entity);
+    }
+
+    public void delete(int id) {
+        userDao.delete(id);
+    }
+
+    public User login(String login, String password) throws UserDoesntExistException{
+        return userDao.login(login, encryptPassword(password));
+    }
+
+    public boolean userExists(String login) {
+        return userDao.userExists(login);
+    }
+
+    public boolean userIsLogged(HttpServletRequest request, String login) {
+        @SuppressWarnings("unchecked")
+        HashSet<String> loggedUsers = (HashSet<String>) request.getSession().getServletContext()
+                .getAttribute("loggedUsers");
+
+        if(loggedUsers.stream().anyMatch(login::equals)){
+            return true;
+        }
+
+        loggedUsers.add(login);
+        request.getSession().getServletContext()
+                .setAttribute("loggedUsers", loggedUsers);
+        return false;
+    }
+
+    private String encryptPassword(String password) {
+        return DigestUtils.md5Hex(password);
+    }
+
+}
